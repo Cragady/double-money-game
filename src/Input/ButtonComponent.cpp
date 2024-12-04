00ad7ada8092f2db381c581b34b8618c38e34a95
Cragady <pinkdotdreamer@gmail.com>
2024-12-04 02:00:31 -0700

    feat: add example of pushing/popping styles

--- src/Input/ButtonComponent.cpp
+++ src/Input/ButtonComponent.cpp
@@ -16,12 +16,22 @@ ButtonComponent::ButtonComponent(std::string name, std::string button_text,
 ButtonComponent::~ButtonComponent() { component_open_ = false; }
 
 void ButtonComponent::Update() {
+
+  float hue = 1 * 0.05f;
+  ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(hue, 0.6f, 0.6f));
+  ImGui::PushStyleColor(ImGuiCol_ButtonHovered,
+                        (ImVec4)ImColor::HSV(hue, 0.7f, 0.7f));
+  ImGui::PushStyleColor(ImGuiCol_ButtonActive,
+                        (ImVec4)ImColor::HSV(hue, 0.8f, 0.8f));
+  ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 5.0f);
   if (ImGui::Begin(name_.c_str(), &component_open_, render_flags_)) {
     if (ImGui::Button(button_text_.c_str())) {
       ClickEvent();
     }
   }
   ImGui::End();
+  ImGui::PopStyleColor(3);
+  ImGui::PopStyleVar();
 }
 
 void ButtonComponent::DefaultEvent() {
