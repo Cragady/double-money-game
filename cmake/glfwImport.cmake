ec580f573a3318f0a3ac66a83b55461bef05f537
Cragady <pinkdotdreamer@gmail.com>
2024-12-04 18:21:15 -0700

    fix: correct build process on new and clean builds

--- cmake/glfwImport.cmake
+++ cmake/glfwImport.cmake
@@ -1,5 +1,13 @@
 if (NOT TARGET glfw)
   project(glfw)
 
+  find_package(Wayland QUIET)
+
+  if (Wayland_FOUND)
+    message(MESSAGE "Wayland found")
+  else()
+    set(GLFW_BUILD_WAYLAND OFF)
+  endif()
+
   add_subdirectory("${CMAKE_SOURCE_DIR}/vendor/glfw")
 endif()
