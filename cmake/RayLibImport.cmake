784d4acf47c9155c8977427be7d224823cbe2018
Cragady <pinkdotdreamer@gmail.com>
2024-12-12 01:29:35 -0700

    feat: add raylib box-collision example

--- cmake/RayLibImport.cmake
+++ cmake/RayLibImport.cmake
@@ -1,8 +1,3 @@
 if (NOT TARGET raylib)
-  find_package(raylib 5.5)
-
-  if (NOT raylib_FOUND)
-    # MESSAGE(WARNING "Building raylib, this will make the raylib submodule dirty")
     add_subdirectory("${CMAKE_SOURCE_DIR}/vendor/raylib" ${BUILD_OUTPUT}/raylib)
-  endif()
 endif()
