298ea1adf8820b687e6b613e4e5cd2ad24768bd7
Cragady <pinkdotdreamer@gmail.com>
2024-12-15 02:31:42 -0700

    chore: comment out unused classes

--- src/DMG/gui/imgui/unused/WindowManager.hpp
+++ src/DMG/gui/imgui/unused/WindowManager.hpp
@@ -1,26 +1,26 @@
-#ifndef DMG_GUI_IMGUI_UNUSED_WINDOW_MANAGER_HPP_
-#define DMG_GUI_IMGUI_UNUSED_WINDOW_MANAGER_HPP_
-
-#include <memory>
-
-#include "DMG/core/GameState.hpp"
-#include "DMG/core/IGuiManager.hpp"
-#include "DMG/gui/imgui/DebugWindow.hpp"
-
-class WindowManager : public IGuiManager {
- public:
-  WindowManager();
-  ~WindowManager();
-
-  void Setup(const GameStateUPtr &, bool) override;
-  void Update(const GameStateUPtr &) override;
-  void Shutdown(const GameStateUPtr &) override;
-
-  bool include_debug_window_ = true;
-  bool debug_window_attached_ = false;
-  std::shared_ptr<DebugWindow> debug_window_;
-};
-
-using WindowManagerSPtr = std::shared_ptr<WindowManager>;
-
-#endif
+// #ifndef DMG_GUI_IMGUI_UNUSED_WINDOW_MANAGER_HPP_
+// #define DMG_GUI_IMGUI_UNUSED_WINDOW_MANAGER_HPP_
+//
+// #include <memory>
+//
+// #include "DMG/core/GameState.hpp"
+// #include "DMG/core/IGuiManager.hpp"
+// #include "DMG/gui/imgui/DebugWindow.hpp"
+//
+// class WindowManager : public IGuiManager {
+//  public:
+//   WindowManager();
+//   ~WindowManager();
+//
+//   void Setup(const GameStateUPtr &, bool) override;
+//   void Update(const GameStateUPtr &) override;
+//   void Shutdown(const GameStateUPtr &) override;
+//
+//   bool include_debug_window_ = true;
+//   bool debug_window_attached_ = false;
+//   std::shared_ptr<DebugWindow> debug_window_;
+// };
+//
+// using WindowManagerSPtr = std::shared_ptr<WindowManager>;
+//
+// #endif
